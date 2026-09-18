43ccaeab91ec0346fd13d3a02fd1889519230bd7
drewd06 <drewdimino06@outlook.com>
2026-09-18 15:32:10 -0500

    Completed Lab04

--- EECE3624-Lab04-main/EECE3624-Lab04-main/Lab04.asm
+++ EECE3624-Lab04-main/EECE3624-Lab04-main/Lab04.asm
@@ -1,13 +1,16 @@
 /**************************************************************************
  *     File: Lab04.asm
- * Lab Name: 
- *   Author: 
- *  Created: 
+ * Lab Name: factN
+ *   Author: Drew Dimino
+ *  Created: 09/15/2026
  *
- * This program...
+ * This program calculates the factorial of an integer N
+ * N must be greater than 0 and less than 6 [1,5]
  *************************************************************************/ 
  .def n = R16
 .def result = R17
+.def np = R18
+.def rp = R19
 .org 0x0000 ; next instruction will be written to address 0x0000
             ; (the location of the reset vector)
 rjmp main	; set reset vector to point to the main code entry point
@@ -20,7 +23,7 @@ main:       ; jump here on reset
 		ldi R16, low(RAMEND)
 		out SPL, R16
 
-		LDI  n, 4	; load a value into n
+		LDI  n, 5; load a value into n
 		PUSH n	; push it on the stack
 		CALL factN	; calculate the factorial of n
 		POP  result	; pop result off stack
@@ -32,11 +35,33 @@ factN:
 	; Comments regarding the factN subroutine go here
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	; recursive factorial code begins here
+	
+	IN YH, SPH	; setting the Y pointer to the stack pointer
+	IN YL, SPL
 
+	LDD np, Y+3	; loading n into temporary register via stack
 
+	CPI np, 1	; base case check (1!)
+	BREQ base
+	
+	DEC np	; decrementing n for recursive call
+	PUSH np	; storing decremented n to stack
 
-recursiveCase:
+	CALL factN	; recursive call
+	
+	POP np	; loading current result into temporary register via stack
 
+	IN YH, SPH	; resetting Y pointer to match stack pointer
+	IN YL, SPL
 
-	ret 
-	; return from the factN subroutine
+	LDD rp, Y+3	; loading next n value to temporary register via stack
+	
+	MUL rp, np	; calculating current result
+	MOV rp, R0	; storing 2B product into temporary result register
+	STD Y+3, rp	; storing current result onto stack
+
+	RET
+
+base:
+	RET
+
